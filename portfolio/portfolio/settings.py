57e72b0f45c9bd68d0e70f5a7674d47d87164eba
progayk <drum.ayk@gmail.com>
2018-03-07 17:52:11 +0300

    install postgresql, create su in psql

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'mayk!jony0',
+        'HOST': '127.0.0.1', # or localhost
+        'PORT': '5432', # you can check it from postgres prompt: \conninfo
     }
 }
 
