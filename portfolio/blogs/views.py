5b42efee9df4d39bc6fea370b52079cb3df87791
progayk <drum.ayk@gmail.com>
2018-03-07 23:41:58 +0300

    polish project and finalize

--- portfolio/blogs/views.py
+++ portfolio/blogs/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
 def allblogs(request):
     blogs = Blog.objects.all()
     return render(request, 'blogs/allblogs.html', {'blogs': blogs})
+
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blogs/blogDetail.html', {'blog_detail': blog_detail})
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -120,10 +120,17 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'portfolio/static/'),
+]
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 
 # tell django where to keep and retrieve from media files
 # when someone wants to save media component, keep them in
